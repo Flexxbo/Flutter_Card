8d3714cea31d6fd19c3f7e5d45978745d9862316
Flexxbo <flexxbo@gmail.com>
2021-02-16 22:08:12 +0100

    layout

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,42 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              height: double.infinity,
+              width: 100.0,
+              color: Colors.red,
+              child: Center(child: Text("1")),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.yellow,
+                  child: Center(child: Text("1")),
+                ),
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.green,
+                  child: Center(child: Text("1")),
+                ),
+              ],
+            ),
+            Container(
+              height: double.infinity,
+              width: 100.0,
+              color: Colors.blue,
+              child: Center(child: Text("1")),
+            ),
+          ],
+        )),
       ),
     );
   }
