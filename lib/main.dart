52103f99fce851bd21e25ac1a54bdf604aed52c2
Flexxbo <flexxbo@gmail.com>
2021-02-17 18:04:32 +0100

    doggy card

--- lib/main.dart
+++ lib/main.dart
@@ -12,76 +12,118 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.purple,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
-                backgroundColor: Colors.amber[900],
-                radius: 70.0,
+                backgroundColor: Colors.amber,
+                radius: 60.0,
                 child: Image(
-                  height: 100.0,
-                  width: 100.0,
-                  image: AssetImage("images/HundeFoto.png"),
+                    image: AssetImage("images/HundeFoto.png"),
+                    height: 90.0,
+                    width: 90.0),
+              ),
+              Text(
+                "Spooner Jr.",
+                style: TextStyle(
+                  color: Colors.amber,
+                  fontFamily: "ReggaeOne",
+                  fontSize: 40.0,
                 ),
               ),
               Text(
-                "Kommissar Rex",
-                style: TextStyle(
-                    fontSize: 30.0,
-                    color: Colors.amber,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: "ReggaeOne"),
-              ),
-              Text(
-                "HAUPTKOMMISSAR HUNDESTAFFEL",
+                "Professional Scout",
                 style: TextStyle(
                   color: Colors.white,
-                  fontSize: 12.5,
-                  letterSpacing: 2,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
                 ),
               ),
               SizedBox(
                 height: 20.0,
-                width: 150.0,
+                width: 100.0,
                 child: Divider(
-                  color: Colors.amber[50],
+                  color: Colors.white,
                 ),
               ),
               Card(
-                color: Colors.white,
-                margin: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 5.0),
-                //padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
-                child: Padding(
-                    padding:
-                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.account_circle,
-                        size: 30.0,
-                        color: Colors.teal[800],
-                      ),
-                      title: Text("Herrchen: Waldemar Waldowski"),
-                    )),
-              ),
-              Container(
-                color: Colors.white,
-                margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 10.0),
-                padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email,
-                      size: 30.0,
-                      color: Colors.teal[800],
+                color: Colors.amber,
+                margin: EdgeInsets.fromLTRB(25.0, 20.0, 25.0, 0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.pan_tool,
+                    color: Colors.white,
+                    size: 30.0,
+                  ),
+                  title: Text(
+                    "Guard Dog",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
                     ),
-                    SizedBox(
-                      width: 15.0,
-                    ),
-                    Text("Snoop@dogmail.com")
-                  ],
+                  ),
                 ),
-              )
+              ),
+              Card(
+                color: Colors.amber,
+                margin: EdgeInsets.fromLTRB(25.0, 20.0, 25.0, 0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.offline_bolt,
+                    color: Colors.white,
+                    size: 30.0,
+                  ),
+                  title: Text(
+                    "Superfast",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.amber,
+                margin: EdgeInsets.fromLTRB(25.0, 20.0, 25.0, 0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.sentiment_satisfied_alt,
+                    color: Colors.white,
+                    size: 30.0,
+                  ),
+                  title: Text(
+                    "Kids Friendly",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.amber,
+                margin: EdgeInsets.fromLTRB(25.0, 20.0, 25.0, 0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.nightlife,
+                    color: Colors.white,
+                    size: 30.0,
+                  ),
+                  title: Text(
+                    "Party Animal",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
@@ -89,17 +131,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-/*
-Row(
-children: <Widget>[
-Icon(
-Icons.account_circle,
-size: 30.0,
-color: Colors.teal[800],
-),
-SizedBox(
-width: 15.0,
-),
-Text("Herrchen: Waldemar Waldowski")
-],
-),*/
